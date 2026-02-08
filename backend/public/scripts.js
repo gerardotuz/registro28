ef92b57036ab601cf23d2a11c2572e44ff3a5833
gerardotuz <82822594+gerardotuz@users.noreply.github.com>
2026-02-07 22:55:10 -0500

    Update scripts.js

--- backend/public/scripts.js
+++ backend/public/scripts.js
@@ -100,8 +100,8 @@ document.addEventListener("DOMContentLoaded", () => {
         cuarta_opcion: toUpper(formData.get("cuarta_opcion")),
       quinta_opcion: toUpper(formData.get("quinta_opcion")),
          sexta_opcion: toUpper(formData.get("sexta_opcion")),
-          quinta_opcion: toUpper(formData.get("septima_opcion")),
-         sexta_opcion: toUpper(formData.get("octava_opcion")),
+          septimaa_opcion: toUpper(formData.get("septima_opcion")),
+         octava_opcion: toUpper(formData.get("octava_opcion")),
 
         estado_nacimiento_general: obtenerClave("estado_nacimiento_general"),
         municipio_nacimiento_general: obtenerClave("municipio_nacimiento_general"),
