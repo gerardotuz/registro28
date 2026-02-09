df80bc22a9203c6195405d7f43eceb5f4c3488d8
gerardotuz <82822594+gerardotuz@users.noreply.github.com>
2026-02-08 20:54:37 -0500

    Fix positioning of boxes for options in PDF generator

--- backend/utils/pdfGenerator.js
+++ backend/utils/pdfGenerator.js
@@ -168,11 +168,11 @@ y += GAP_Y;
 y = drawBox('Cuarta Opción', generales.cuarta_opcion, marginX, y);
 y = drawBox('Quinta Opción', generales.quinta_opcion, marginX + 260, y);
 y += GAP_Y;
-  y = drawBox(' ', generales._opcion, marginX, y);
-y = drawBox('Sexta Opción', generales.sexta_opcion, marginX + 260, y);
+  y = drawBox('Sexta Opción', generales.sexta_opcion, marginX, y);
+y = drawBox('Septima Opción', generales.septima_opcion, marginX + 260, y);
 y += GAP_Y;
-  y = drawBox('Septima Opción', generales.septima_opcion, marginX, y);
-y = drawBox('Octava Opción', generales.octava_opcion, marginX + 260, y);
+  y = drawBox('Octava Opción', generales.octava_opcion, marginX, y);
+y = drawBox('Novena Opción', generales.novena_opcion, marginX + 260, y);
 y += GAP_Y;
 
 
